dda6c9df987a36bca3f02bae1fb25b0d575b781e
JRSasan <sasan.jeanrae@gmail.com>
2023-12-20 09:54:33 +0800

    initial database set up

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+    databaseURL: "https://adaydb-8efe8-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const expenseListInDB = ref(database, "expenseList");
 
 const descriptionFieldEl = document.getElementById("description-input");
 const priceFieldEl = document.getElementById("price-input");
@@ -6,7 +16,8 @@ const addButtonEl = document.getElementById("add-button");
 addButtonEl.addEventListener("click", function() {
     let descriptionValue = descriptionFieldEl.value;
     let priceValue = priceFieldEl.value;
-    console.log(descriptionValue);
-    console.log(priceValue);
+    let expense = {description: descriptionValue, price: priceValue};
+
+    push(shoppingListInDB, expense);
 });
 
